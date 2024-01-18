07e1781dd1d5cd79ed929233cf8ce59397d0c070
Jacky Liu <87030427+jacky1234-data@users.noreply.github.com>
2024-01-18 16:07:48 -0500

    Add files via upload

---
+++ static/javacode.js
@@ -0,0 +1,10 @@
+/*
+function show(){
+    var k_val = document.getElementById("k_values");
+    k_val = parseInt(k_val);
+    if (k_val==2){
+        $('#xC_val').slider({ disabled: "true" });
+        $('#yC_val').slider({ disabled: "true" });
+    }
+}
+*/
